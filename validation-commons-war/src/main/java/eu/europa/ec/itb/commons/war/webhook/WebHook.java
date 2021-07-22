a33a73bc5997fa39c7f12ff12fa0fa0c44f1b52f
Costas Simatos <costas.simatos@sigmacubed.eu>
2021-07-22 17:58:48 +0200

    Code formatting.

--- validation-commons-war/src/main/java/eu/europa/ec/itb/commons/war/webhook/WebHook.java
+++ validation-commons-war/src/main/java/eu/europa/ec/itb/commons/war/webhook/WebHook.java
@@ -35,80 +35,80 @@ import eu.europa.ec.itb.validation.commons.config.ApplicationConfig;
 @ConditionalOnProperty(name = "validator.webhook.statistics")
 public class WebHook {
 
-	private static final Logger logger = LoggerFactory.getLogger(WebHook.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebHook.class);
 
-	@Autowired
-	private ApplicationConfig config;
+    @Autowired
+    private ApplicationConfig config;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	private ObjectWriter usageDataWriter;
-	
-	private RestTemplate restTemplate;
+    private ObjectWriter usageDataWriter;
 
-	private String url;
+    private RestTemplate restTemplate;
 
-	private String secret;
+    private String url;
 
-	/**
-	 * Method called to initialize the webhook attributes.
-	 *
-	 * @throws MalformedURLException If the webhook URL is invalid.
-	 */
-	@PostConstruct
-	public void initializeAttributes() throws MalformedURLException {
-		// Instantiation of the rest template object to perform the calls
-		this.restTemplate = new RestTemplate();
-		// Gathering the properties to report usage
-		ApplicationConfig.Webhook webhookConfig = config.getWebhook();
-		this.url = webhookConfig.getStatistics();
-		try {
-			new URL(this.url);
-		}catch(Exception ex) {
-			throw new MalformedURLException(
-					"The following URL for the usage statistics service is not valid: " + this.url);
-		}
-		this.secret = webhookConfig.getStatisticsSecret();
-		if(this.secret == null){
-			this.usageDataWriter = objectMapper.writerFor(UsageData.class);
-		}else{
-			this.usageDataWriter = objectMapper.writerFor(UsageData.class).withAttribute("secret", this.secret);
-		}
-		logger.info(String.format("Statistics reporting is active and set to post data to [%s]", this.url));
-	}
-	
+    private String secret;
 
-	/**
-	 * Method that posts the current validation details to the usage statistics
-	 * service. This method is called asynchronously to not block the validation result.
-	 * 
-	 * @param data The data to post.
-	 */
-	@Async
-	public void sendUsageData(UsageData data) {
-		Map<String, String> urlParams = new HashMap<String, String>();
-		if (this.secret != null) {
-			urlParams.put("secret", this.secret);
-		}
-		String jsonData;
-		try {
-			jsonData = usageDataWriter.writeValueAsString(data);
-			logger.debug("Sending usage data:\n {}", jsonData);
-			HttpHeaders headers = new HttpHeaders();
-			headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
-			HttpEntity<String> entity = new HttpEntity<>(jsonData, headers);
-			try{
-				ResponseEntity<String> response = restTemplate.postForEntity(this.url, entity, String.class, urlParams);
-				if(response.getStatusCodeValue() != 200) {
-					logger.warn("Statistics reporting received response " + response.getStatusCodeValue() + "and message " + response.getBody());
-				}
-			}catch(Exception ex){
-				logger.warn("Error during statistics reporting", ex);
-			}
-		} catch (JsonProcessingException ex) {
-			logger.warn("Error serializing UsageData object to JSON format.", ex);
-		}
-	}
+    /**
+     * Method called to initialize the webhook attributes.
+     *
+     * @throws MalformedURLException If the webhook URL is invalid.
+     */
+    @PostConstruct
+    public void initializeAttributes() throws MalformedURLException {
+        // Instantiation of the rest template object to perform the calls
+        this.restTemplate = new RestTemplate();
+        // Gathering the properties to report usage
+        ApplicationConfig.Webhook webhookConfig = config.getWebhook();
+        this.url = webhookConfig.getStatistics();
+        try {
+            new URL(this.url);
+        }catch(Exception ex) {
+            throw new MalformedURLException(
+                    "The following URL for the usage statistics service is not valid: " + this.url);
+        }
+        this.secret = webhookConfig.getStatisticsSecret();
+        if(this.secret == null){
+            this.usageDataWriter = objectMapper.writerFor(UsageData.class);
+        }else{
+            this.usageDataWriter = objectMapper.writerFor(UsageData.class).withAttribute("secret", this.secret);
+        }
+        logger.info(String.format("Statistics reporting is active and set to post data to [%s]", this.url));
+    }
+
+
+    /**
+     * Method that posts the current validation details to the usage statistics
+     * service. This method is called asynchronously to not block the validation result.
+     *
+     * @param data The data to post.
+     */
+    @Async
+    public void sendUsageData(UsageData data) {
+        Map<String, String> urlParams = new HashMap<String, String>();
+        if (this.secret != null) {
+            urlParams.put("secret", this.secret);
+        }
+        String jsonData;
+        try {
+            jsonData = usageDataWriter.writeValueAsString(data);
+            logger.debug("Sending usage data:\n {}", jsonData);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+            HttpEntity<String> entity = new HttpEntity<>(jsonData, headers);
+            try{
+                ResponseEntity<String> response = restTemplate.postForEntity(this.url, entity, String.class, urlParams);
+                if(response.getStatusCodeValue() != 200) {
+                    logger.warn("Statistics reporting received response " + response.getStatusCodeValue() + "and message " + response.getBody());
+                }
+            }catch(Exception ex){
+                logger.warn("Error during statistics reporting", ex);
+            }
+        } catch (JsonProcessingException ex) {
+            logger.warn("Error serializing UsageData object to JSON format.", ex);
+        }
+    }
 
 }
