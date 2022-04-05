75493cbfb4beed1342e51641c957919be2d6fa89
simatosc <costas.simatos@sigmacubed.eu>
2022-04-05 13:30:52 +0200

    Quality improvements

--- validation-commons/src/test/java/eu/europa/ec/itb/validation/commons/ReportPairTest.java
+++ validation-commons/src/test/java/eu/europa/ec/itb/validation/commons/ReportPairTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ReportPairTest {
+class ReportPairTest {
 
     @Test
     void testAssignments() {
