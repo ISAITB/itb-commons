5012353626ef7ae2751a43e90349fda5bb73bda7
simatosc <costas.simatos@sigmacubed.eu>
2022-05-09 16:20:21 +0200

    [ITB-1210] Switch to JS-based report

---
+++ validation-commons-web/src/main/java/eu/europa/ec/itb/validation/commons/web/dto/Translations.java
@@ -0,0 +1,538 @@
+package eu.europa.ec.itb.validation.commons.web.dto;
+
+import com.gitb.tr.TAR;
+import eu.europa.ec.itb.validation.commons.LocalisationHelper;
+import eu.europa.ec.itb.validation.commons.config.DomainConfig;
+
+import java.util.Locale;
+
+/**
+ * Class to hold translations for display on the web UI following a validation.
+ *
+ * This class holds text translations that are common to all validators.
+ */
+public class Translations {
+
+    private String resultSectionTitle;
+    private String resultSubSectionOverviewTitle;
+    private String resultDateLabel;
+    private String resultFileNameLabel;
+    private String resultValidationTypeLabel;
+    private String resultResultLabel;
+    private String resultValue;
+    private String resultErrorsLabel;
+    private String resultWarningsLabel;
+    private String resultMessagesLabel;
+    private String resultSubSectionDetailsTitle;
+    private String viewReportItemsShowAll;
+    private String viewReportItemsShowErrors;
+    private String viewReportItemsShowWarnings;
+    private String viewReportItemsShowMessages;
+    private String viewReportItemsDetailed;
+    private String viewReportItemsAggregated;
+    private String maximumReportsExceededForDetailedOutputMessage;
+    private String maximumReportsExceededForXmlOutputMessage;
+    private String resultLocationLabel;
+    private String resultTestLabel;
+    private String additionalInfoLabel;
+    private String popupCloseButton;
+    private String popupTitle;
+    private String reportDetailedPDF;
+    private String reportDetailedCSV;
+    private String reportAggregatedPDF;
+    private String reportAggregatedCSV;
+    private String viewAnnotatedInputButton;
+    private String downloadReportButton;
+    private String reportDetailedXML;
+    private String reportAggregatedXML;
+
+    /**
+     * Constructor that set's all labels that are common for all validators.
+     *
+     * @param helper The helper class to facilitate localisation.
+     * @param report The (detailed) TAR validation report.
+     * @param domainConfig The relevant domain configuration.
+     */
+    public Translations(LocalisationHelper helper, TAR report, DomainConfig domainConfig) {
+        setResultSectionTitle(helper.localise("validator.label.resultSectionTitle"));
+        setResultValue(helper.localise("validator.label.result."+report.getResult().value().toLowerCase(Locale.ROOT)));
+        setResultErrorsLabel(helper.localise("validator.label.resultErrorsLabel"));
+        setResultWarningsLabel(helper.localise("validator.label.resultWarningsLabel"));
+        setResultMessagesLabel(helper.localise("validator.label.resultMessagesLabel"));
+        setResultDateLabel(helper.localise("validator.label.resultDateLabel"));
+        setResultFileNameLabel(helper.localise("validator.label.resultFileNameLabel"));
+        setResultValidationTypeLabel(helper.localise("validator.label.resultValidationTypeLabel"));
+        setResultResultLabel(helper.localise("validator.label.resultResultLabel"));
+        setResultSubSectionDetailsTitle(helper.localise("validator.label.resultSubSectionDetailsTitle"));
+        setResultSubSectionOverviewTitle(helper.localise("validator.label.resultSubSectionOverviewTitle"));
+        setViewReportItemsShowAll(helper.localise("validator.label.viewReportItemsShowAll"));
+        setViewReportItemsShowErrors(helper.localise("validator.label.viewReportItemsShowErrors"));
+        setViewReportItemsShowWarnings(helper.localise("validator.label.viewReportItemsShowWarnings"));
+        setViewReportItemsShowMessages(helper.localise("validator.label.viewReportItemsShowMessages"));
+        setViewReportItemsDetailed(helper.localise("validator.label.viewReportItemsDetailed"));
+        setViewReportItemsAggregated(helper.localise("validator.label.viewReportItemsAggregated"));
+        setMaximumReportsExceededForDetailedOutputMessage(helper.localise("validator.label.maximumReportsExceededForDetailedOutputMessage"));
+        setResultLocationLabel(helper.localise("validator.label.resultLocationLabel"));
+        setResultTestLabel(helper.localise("validator.label.resultTestLabel"));
+        setAdditionalInfoLabel(helper.localise("validator.label.additionalInfoLabel"));
+        setPopupCloseButton(helper.localise("validator.label.popupCloseButton"));
+        setPopupTitle(helper.localise("validator.label.popupTitle"));
+        setReportDetailedPDF(helper.localise("validator.label.reportDetailedPDF"));
+        setReportDetailedCSV(helper.localise("validator.label.reportDetailedCSV"));
+        setReportAggregatedPDF(helper.localise("validator.label.reportAggregatedPDF"));
+        setReportAggregatedCSV(helper.localise("validator.label.reportAggregatedCSV"));
+        setViewAnnotatedInputButton(helper.localise("validator.label.viewAnnotatedInputButton"));
+        setDownloadReportButton(helper.localise("validator.label.downloadReportButton"));
+        setReportDetailedXML(helper.localise("validator.label.reportDetailedXML"));
+        setReportAggregatedXML(helper.localise("validator.label.reportAggregatedXML"));
+        setMaximumReportsExceededForXmlOutputMessage(helper.localise("validator.label.maximumReportsExceededForXmlOutputMessage", domainConfig.getMaximumReportsForXmlOutput()));
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultSectionTitle() {
+        return resultSectionTitle;
+    }
+
+    /**
+     * @param resultSectionTitle The label value to set.
+     */
+    public void setResultSectionTitle(String resultSectionTitle) {
+        this.resultSectionTitle = resultSectionTitle;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultSubSectionOverviewTitle() {
+        return resultSubSectionOverviewTitle;
+    }
+
+    /**
+     * @param resultSubSectionOverviewTitle The label value to set.
+     */
+    public void setResultSubSectionOverviewTitle(String resultSubSectionOverviewTitle) {
+        this.resultSubSectionOverviewTitle = resultSubSectionOverviewTitle;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultDateLabel() {
+        return resultDateLabel;
+    }
+
+    /**
+     * @param resultDateLabel The label value to set.
+     */
+    public void setResultDateLabel(String resultDateLabel) {
+        this.resultDateLabel = resultDateLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultFileNameLabel() {
+        return resultFileNameLabel;
+    }
+
+    /**
+     * @param resultFileNameLabel The label value to set.
+     */
+    public void setResultFileNameLabel(String resultFileNameLabel) {
+        this.resultFileNameLabel = resultFileNameLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultValidationTypeLabel() {
+        return resultValidationTypeLabel;
+    }
+
+    /**
+     * @param resultValidationTypeLabel The label value to set.
+     */
+    public void setResultValidationTypeLabel(String resultValidationTypeLabel) {
+        this.resultValidationTypeLabel = resultValidationTypeLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultResultLabel() {
+        return resultResultLabel;
+    }
+
+    /**
+     * @param resultResultLabel The label value to set.
+     */
+    public void setResultResultLabel(String resultResultLabel) {
+        this.resultResultLabel = resultResultLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultValue() {
+        return resultValue;
+    }
+
+    /**
+     * @param resultValue The label value to set.
+     */
+    public void setResultValue(String resultValue) {
+        this.resultValue = resultValue;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultErrorsLabel() {
+        return resultErrorsLabel;
+    }
+
+    /**
+     * @param resultErrorsLabel The label value to set.
+     */
+    public void setResultErrorsLabel(String resultErrorsLabel) {
+        this.resultErrorsLabel = resultErrorsLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultWarningsLabel() {
+        return resultWarningsLabel;
+    }
+
+    /**
+     * @param resultWarningsLabel The label value to set.
+     */
+    public void setResultWarningsLabel(String resultWarningsLabel) {
+        this.resultWarningsLabel = resultWarningsLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultMessagesLabel() {
+        return resultMessagesLabel;
+    }
+
+    /**
+     * @param resultMessagesLabel The label value to set.
+     */
+    public void setResultMessagesLabel(String resultMessagesLabel) {
+        this.resultMessagesLabel = resultMessagesLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultSubSectionDetailsTitle() {
+        return resultSubSectionDetailsTitle;
+    }
+
+    /**
+     * @param resultSubSectionDetailsTitle The label value to set.
+     */
+    public void setResultSubSectionDetailsTitle(String resultSubSectionDetailsTitle) {
+        this.resultSubSectionDetailsTitle = resultSubSectionDetailsTitle;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsShowAll() {
+        return viewReportItemsShowAll;
+    }
+
+    /**
+     * @param viewReportItemsShowAll The label value to set.
+     */
+    public void setViewReportItemsShowAll(String viewReportItemsShowAll) {
+        this.viewReportItemsShowAll = viewReportItemsShowAll;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsShowErrors() {
+        return viewReportItemsShowErrors;
+    }
+
+    /**
+     * @param viewReportItemsShowErrors The label value to set.
+     */
+    public void setViewReportItemsShowErrors(String viewReportItemsShowErrors) {
+        this.viewReportItemsShowErrors = viewReportItemsShowErrors;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsShowWarnings() {
+        return viewReportItemsShowWarnings;
+    }
+
+    /**
+     * @param viewReportItemsShowWarnings The label value to set.
+     */
+    public void setViewReportItemsShowWarnings(String viewReportItemsShowWarnings) {
+        this.viewReportItemsShowWarnings = viewReportItemsShowWarnings;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsShowMessages() {
+        return viewReportItemsShowMessages;
+    }
+
+    /**
+     * @param viewReportItemsShowMessages The label value to set.
+     */
+    public void setViewReportItemsShowMessages(String viewReportItemsShowMessages) {
+        this.viewReportItemsShowMessages = viewReportItemsShowMessages;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsDetailed() {
+        return viewReportItemsDetailed;
+    }
+
+    /**
+     * @param viewReportItemsDetailed The label value to set.
+     */
+    public void setViewReportItemsDetailed(String viewReportItemsDetailed) {
+        this.viewReportItemsDetailed = viewReportItemsDetailed;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewReportItemsAggregated() {
+        return viewReportItemsAggregated;
+    }
+
+    /**
+     * @param viewReportItemsAggregated The label value to set.
+     */
+    public void setViewReportItemsAggregated(String viewReportItemsAggregated) {
+        this.viewReportItemsAggregated = viewReportItemsAggregated;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getMaximumReportsExceededForDetailedOutputMessage() {
+        return maximumReportsExceededForDetailedOutputMessage;
+    }
+
+    /**
+     * @param maximumReportsExceededForDetailedOutputMessage The label value to set.
+     */
+    public void setMaximumReportsExceededForDetailedOutputMessage(String maximumReportsExceededForDetailedOutputMessage) {
+        this.maximumReportsExceededForDetailedOutputMessage = maximumReportsExceededForDetailedOutputMessage;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultLocationLabel() {
+        return resultLocationLabel;
+    }
+
+    /**
+     * @param resultLocationLabel The label value to set.
+     */
+    public void setResultLocationLabel(String resultLocationLabel) {
+        this.resultLocationLabel = resultLocationLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getResultTestLabel() {
+        return resultTestLabel;
+    }
+
+    /**
+     * @param resultTestLabel The label value to set.
+     */
+    public void setResultTestLabel(String resultTestLabel) {
+        this.resultTestLabel = resultTestLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getAdditionalInfoLabel() {
+        return additionalInfoLabel;
+    }
+
+    /**
+     * @param additionalInfoLabel The label value to set.
+     */
+    public void setAdditionalInfoLabel(String additionalInfoLabel) {
+        this.additionalInfoLabel = additionalInfoLabel;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getPopupCloseButton() {
+        return popupCloseButton;
+    }
+
+    /**
+     * @param popupCloseButton The label value to set.
+     */
+    public void setPopupCloseButton(String popupCloseButton) {
+        this.popupCloseButton = popupCloseButton;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getPopupTitle() {
+        return popupTitle;
+    }
+
+    /**
+     * @param popupTitle The label value to set.
+     */
+    public void setPopupTitle(String popupTitle) {
+        this.popupTitle = popupTitle;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportDetailedPDF() {
+        return reportDetailedPDF;
+    }
+
+    /**
+     * @param reportDetailedPDF The label value to set.
+     */
+    public void setReportDetailedPDF(String reportDetailedPDF) {
+        this.reportDetailedPDF = reportDetailedPDF;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportAggregatedPDF() {
+        return reportAggregatedPDF;
+    }
+
+    /**
+     * @param reportAggregatedPDF The label value to set.
+     */
+    public void setReportAggregatedPDF(String reportAggregatedPDF) {
+        this.reportAggregatedPDF = reportAggregatedPDF;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportDetailedCSV() {
+        return reportDetailedCSV;
+    }
+
+    /**
+     * @param reportDetailedCSV The label value to set.
+     */
+    public void setReportDetailedCSV(String reportDetailedCSV) {
+        this.reportDetailedCSV = reportDetailedCSV;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportAggregatedCSV() {
+        return reportAggregatedCSV;
+    }
+
+    /**
+     * @param reportAggregatedCSV The label value to set.
+     */
+    public void setReportAggregatedCSV(String reportAggregatedCSV) {
+        this.reportAggregatedCSV = reportAggregatedCSV;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getMaximumReportsExceededForXmlOutputMessage() {
+        return maximumReportsExceededForXmlOutputMessage;
+    }
+
+    /**
+     * @param maximumReportsExceededForXmlOutputMessage The label value to set.
+     */
+    public void setMaximumReportsExceededForXmlOutputMessage(String maximumReportsExceededForXmlOutputMessage) {
+        this.maximumReportsExceededForXmlOutputMessage = maximumReportsExceededForXmlOutputMessage;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getViewAnnotatedInputButton() {
+        return viewAnnotatedInputButton;
+    }
+
+    /**
+     * @param viewAnnotatedInputButton The label value to set.
+     */
+    public void setViewAnnotatedInputButton(String viewAnnotatedInputButton) {
+        this.viewAnnotatedInputButton = viewAnnotatedInputButton;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getDownloadReportButton() {
+        return downloadReportButton;
+    }
+
+    /**
+     * @param downloadReportButton The label value to set.
+     */
+    public void setDownloadReportButton(String downloadReportButton) {
+        this.downloadReportButton = downloadReportButton;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportDetailedXML() {
+        return reportDetailedXML;
+    }
+
+    /**
+     * @param reportDetailedXML The label value to set.
+     */
+    public void setReportDetailedXML(String reportDetailedXML) {
+        this.reportDetailedXML = reportDetailedXML;
+    }
+
+    /**
+     * @return The label value.
+     */
+    public String getReportAggregatedXML() {
+        return reportAggregatedXML;
+    }
+
+    /**
+     * @param reportAggregatedXML The label value to set..
+     */
+    public void setReportAggregatedXML(String reportAggregatedXML) {
+        this.reportAggregatedXML = reportAggregatedXML;
+    }
+}
---
+++ validation-commons-web/src/main/java/eu/europa/ec/itb/validation/commons/web/dto/UploadResult.java
@@ -0,0 +1,210 @@
+package eu.europa.ec.itb.validation.commons.web.dto;
+
+import com.gitb.tr.TAR;
+import eu.europa.ec.itb.validation.commons.LocalisationHelper;
+import eu.europa.ec.itb.validation.commons.Utils;
+import eu.europa.ec.itb.validation.commons.config.WebDomainConfig;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Class used to collect the values for the result of a validation via the user interface. This class is serialised
+ * to JSON as the response to the validation call.
+ *
+ * @param <T> The specific translation type for the validator in question.
+ */
+public class UploadResult <T extends Translations> {
+
+    private String message;
+    private String reportId;
+    private String fileName;
+    private TAR report;
+    private TAR aggregateReport;
+    private boolean showAggregateReport;
+    private String date;
+    private String validationTypeLabel;
+    private Long maximumReportsForDetailedOutput;
+    private Long maximumReportsForXmlOutput;
+    private T translations;
+
+    /**
+     * @return The (optional) error message to display.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message The (optional) error message to display.
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return The identifier of the validation report to be used in relevant REST calls.
+     */
+    public String getReportId() {
+        return reportId;
+    }
+
+    /**
+     * @param reportId The identifier of the validation report to be used in relevant REST calls.
+     */
+    public void setReportId(String reportId) {
+        this.reportId = reportId;
+    }
+
+    /**
+     * @return The file name to display on the user interface for the validated input.
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * @param fileName The file name to display on the user interface for the validated input.
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * @return The detailed TAR validation report.
+     */
+    public TAR getReport() {
+        return report;
+    }
+
+    /**
+     * @param report The detailed TAR validation report.
+     */
+    public void setReport(TAR report) {
+        this.report = report;
+    }
+
+    /**
+     * @return The aggregated TAR validation report.
+     */
+    public TAR getAggregateReport() {
+        return aggregateReport;
+    }
+
+    /**
+     * @param aggregateReport The aggregated TAR validation report.
+     */
+    public void setAggregateReport(TAR aggregateReport) {
+        this.aggregateReport = aggregateReport;
+    }
+
+    /**
+     * @return Whether to display the aggregate report.
+     */
+    public boolean isShowAggregateReport() {
+        return showAggregateReport;
+    }
+
+    /**
+     * @param showAggregateReport Whether to display the aggregate report.
+     */
+    public void setShowAggregateReport(boolean showAggregateReport) {
+        this.showAggregateReport = showAggregateReport;
+    }
+
+    /**
+     * @return The validation timestamp.
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date The validation timestamp.
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * @return The complete label to display for the type of validation performed.
+     */
+    public String getValidationTypeLabel() {
+        return validationTypeLabel;
+    }
+
+    /**
+     * @param validationTypeLabel The complete label to display for the type of validation performed.
+     */
+    public void setValidationTypeLabel(String validationTypeLabel) {
+        this.validationTypeLabel = validationTypeLabel;
+    }
+
+    /**
+     * @return The maximum number of report items allowed in the on-screen report.
+     */
+    public Long getMaximumReportsForDetailedOutput() {
+        return maximumReportsForDetailedOutput;
+    }
+
+    /**
+     * @param maximumReportsForDetailedOutput The maximum number of report items allowed in the on-screen report.
+     */
+    public void setMaximumReportsForDetailedOutput(Long maximumReportsForDetailedOutput) {
+        this.maximumReportsForDetailedOutput = maximumReportsForDetailedOutput;
+    }
+
+    /**
+     * @return The maximum number of report items included in the XML validation report.
+     */
+    public Long getMaximumReportsForXmlOutput() {
+        return maximumReportsForXmlOutput;
+    }
+
+    /**
+     * @param maximumReportsForXmlOutput The maximum number of report items included in the XML validation report.
+     */
+    public void setMaximumReportsForXmlOutput(Long maximumReportsForXmlOutput) {
+        this.maximumReportsForXmlOutput = maximumReportsForXmlOutput;
+    }
+
+    /**
+     * @return The translation texts to use in the user interface.
+     */
+    public T getTranslations() {
+        return translations;
+    }
+
+    /**
+     * @param translations The translation texts to use in the user interface.
+     */
+    public void setTranslations(T translations) {
+        this.translations = translations;
+    }
+
+    /**
+     * Populate all result properties that are common to all validator types.
+     *
+     * @param helper The localisation helper to use to lookup translations.
+     * @param validationType The current validation type.
+     * @param domainConfig The domain configuration.
+     * @param reportId The report identifier.
+     * @param fileName The fime name to display.
+     * @param detailedReport The detailed TAR report.
+     * @param aggregateReport The aggregated report.
+     * @param translations The translations to use.
+     */
+    public void populateCommon(LocalisationHelper helper, String validationType, WebDomainConfig domainConfig, String reportId,
+                               String fileName, TAR detailedReport, TAR aggregateReport, T translations) {
+        if (StringUtils.isNotBlank(validationType)) {
+            setValidationTypeLabel(domainConfig.getCompleteTypeOptionLabel(validationType, helper));
+        }
+        setMaximumReportsForDetailedOutput(domainConfig.getMaximumReportsForDetailedOutput());
+        setMaximumReportsForXmlOutput(domainConfig.getMaximumReportsForXmlOutput());
+        setReportId(reportId);
+        setFileName(fileName);
+        setReport(detailedReport);
+        setAggregateReport(aggregateReport);
+        setShowAggregateReport(Utils.aggregateDiffers(detailedReport, aggregateReport));
+        setDate(detailedReport.getDate().toString());
+        setTranslations(translations);
+    }
+}
