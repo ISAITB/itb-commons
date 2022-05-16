b1c71722284b3e544d4782ecd7a8be9cf5e4e83a
simatosc <costas.simatos@sigmacubed.eu>
2022-05-16 14:46:55 +0200

    [ITB-1215] Unite tests

--- validation-commons-war/src/test/java/eu/europa/ec/itb/validation/commons/war/config/SecurityConfigEmbeddingTest.java
+++ validation-commons-war/src/test/java/eu/europa/ec/itb/validation/commons/war/config/SecurityConfigEmbeddingTest.java
@@ -1,6 +1,7 @@
 package eu.europa.ec.itb.validation.commons.war.config;
 
 import eu.europa.ec.itb.validation.commons.config.ApplicationConfig;
+import eu.europa.ec.itb.validation.commons.config.WebDomainConfig;
 import eu.europa.ec.itb.validation.commons.config.WebDomainConfigCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import static org.hamcrest.Matchers.containsString;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
-@ContextConfiguration(classes = { SecurityConfigTest.TestConfig.class, SecurityConfig.class })
-class SecurityConfigTest {
+@ContextConfiguration(classes = { SecurityConfigEmbeddingTest.TestConfig.class, SecurityConfig.class })
+class SecurityConfigEmbeddingTest {
 
     @TestConfiguration
     static class TestConfig {
@@ -36,17 +39,18 @@ class SecurityConfigTest {
         }
         @Bean
         WebDomainConfigCache<?> webDomainConfigCache() {
-            return mock(WebDomainConfigCache.class);
+            var cache = mock(WebDomainConfigCache.class);
+            var domainConfig = mock(WebDomainConfig.class);
+            when(domainConfig.isSupportUserInterfaceEmbedding()).thenReturn(true);
+            when(cache.getAllDomainConfigurations()).thenAnswer(call -> List.of(domainConfig));
+            when(cache.getConfigForDomainName(anyString())).thenReturn(domainConfig);
+            return cache;
         }
         @Controller
         static class TestController {
             @RequestMapping(method= RequestMethod.GET, path = "/test1/upload")
             public @ResponseBody String test1() {
                 return "TEST1";
-            }
-            @RequestMapping(method= RequestMethod.GET, path = "/test2/upload")
-            public @ResponseBody String test2() {
-                return "TEST2";
             }
         }
     }
@@ -59,26 +63,7 @@ class SecurityConfigTest {
         mvc.perform(get("/test1/upload"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("TEST1"))
-                .andExpect(header().string("X-Frame-Options", "DENY"));
-
-    }
-
-    @Test
-    void testCORS() throws Exception {
-        mvc.perform(options("/test1/upload")
-                .header("Access-Control-Request-Method", "GET")
-                .header("Origin", "http://a.domain.com"))
-                .andExpect(status().isOk())
-                .andExpect(header().exists("Access-Control-Allow-Origin"))
-                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
-                .andExpect(header().exists("Access-Control-Allow-Methods"))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("OPTIONS")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("HEAD")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("GET")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("POST")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("PUT")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("DELETE")))
-                .andExpect(header().string("Access-Control-Allow-Methods", containsString("PATCH")));
+                .andExpect(header().doesNotExist("X-Frame-Options"));
     }
 
 }
