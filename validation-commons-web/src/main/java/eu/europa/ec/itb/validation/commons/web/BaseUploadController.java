ce85a35374337365be1c311d1e050475c7ef9dcc
simatosc <costas.simatos@sigmacubed.eu>
2022-05-17 11:11:12 +0200

    [ITB-1215] Refactoring and unit testing

--- validation-commons-web/src/main/java/eu/europa/ec/itb/validation/commons/web/BaseUploadController.java
+++ validation-commons-web/src/main/java/eu/europa/ec/itb/validation/commons/web/BaseUploadController.java
@@ -5,15 +5,27 @@ import eu.europa.ec.itb.validation.commons.config.DomainConfigCache;
 import eu.europa.ec.itb.validation.commons.config.WebDomainConfig;
 import eu.europa.ec.itb.validation.commons.web.dto.UploadResult;
 import eu.europa.ec.itb.validation.commons.web.errors.NotFoundException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Objects;
+
+import static eu.europa.ec.itb.validation.commons.web.Constants.IS_MINIMAL;
 
 /**
  * Abstract super class of all controllers for validator UI forms.
  */
 public abstract class BaseUploadController <X extends WebDomainConfig, Y extends DomainConfigCache<X>> {
+
+    /** The content type value in case of a file upload. */
+    public static final String CONTENT_TYPE_FILE = "fileType" ;
+    /** The content type value in case of a provided URI. */
+    public static final String CONTENT_TYPE_URI = "uriType" ;
+    /** The content type value in case of text from an editor. */
+    public static final String CONTENT_TYPE_STRING = "stringType" ;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -47,4 +59,57 @@ public abstract class BaseUploadController <X extends WebDomainConfig, Y extends
         }
     }
 
+    /**
+     * Check and/or determine the input submission type based on the received information.
+     * This will fail if the content type could not be determined.
+     *
+     * @param contentType The declared content type.
+     * @param file The received file input (or null).
+     * @param uri The received URI input (or null).
+     * @param string The received text input (or null).
+     * @return The content type value to consider.
+     */
+    public String checkInputType(String contentType, MultipartFile file, String uri, String string) {
+        return checkInputType(contentType, file, uri, string, true);
+    }
+
+    /**
+     * Check and/or determine the input submission type based on the received information.
+     *
+     * @param contentType The declared content type.
+     * @param file The received file input (or null).
+     * @param uri The received URI input (or null).
+     * @param string The received text input (or null).
+     * @param failIfUndetermined Raise an error if the content type could not be determined.
+     * @return The content type value to consider.
+     */
+    public String checkInputType(String contentType, MultipartFile file, String uri, String string, boolean failIfUndetermined) {
+        if (StringUtils.isEmpty(contentType)) {
+            var fileProvided = file != null;
+            var uriProvided = StringUtils.isNotEmpty(uri);
+            var textProvided = StringUtils.isNotEmpty(string);
+            if (fileProvided && !uriProvided && !textProvided) {
+                contentType = CONTENT_TYPE_FILE;
+            } else if (!fileProvided && uriProvided && !textProvided) {
+                contentType = CONTENT_TYPE_URI;
+            } else if (!fileProvided && !uriProvided && textProvided) {
+                contentType = CONTENT_TYPE_STRING;
+            } else if (failIfUndetermined) {
+                throw new IllegalArgumentException("No explicit content type was declared and determining it from the provided inputs was not possible.");
+            }
+        }
+        return contentType;
+    }
+
+    /**
+     * Check to see if the UI should be displayed as minimal.
+     *
+     * @see eu.europa.ec.itb.validation.commons.web.filters.DomainCheckFilter
+     *
+     * @param request The current request.
+     * @return The check result.
+     */
+    public boolean isMinimalUI(HttpServletRequest request) {
+        return Objects.requireNonNullElse((Boolean) request.getAttribute(IS_MINIMAL), Boolean.FALSE);
+    }
 }
--- validation-commons-web/src/test/java/eu/europa/ec/itb/validation/commons/web/BaseUploadControllerTest.java
+++ validation-commons-web/src/test/java/eu/europa/ec/itb/validation/commons/web/BaseUploadControllerTest.java
@@ -7,11 +7,13 @@ import eu.europa.ec.itb.validation.commons.web.dto.UploadResult;
 import eu.europa.ec.itb.validation.commons.web.errors.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.Writer;
 
+import static eu.europa.ec.itb.validation.commons.web.Constants.IS_MINIMAL;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -63,6 +65,47 @@ class BaseUploadControllerTest {
         var request = mock(HttpServletRequest.class);
         when(request.getAttribute(anyString())).thenReturn(null);
         assertThrows(NotFoundException.class, () -> controller.getDomainConfig(request));
+    }
+
+    @Test
+    void testCheckInputType() {
+        // No input.
+        assertThrows(IllegalArgumentException.class, () -> assertNull(controller.checkInputType(null, null, null, null)));
+        assertThrows(IllegalArgumentException.class, () -> controller.checkInputType(null, mock(MultipartFile.class), "http://uri", "STRING"));
+        // Content type provided.
+        assertEquals("CONTENT_TYPE", controller.checkInputType("CONTENT_TYPE", null, null, null));
+        assertEquals("CONTENT_TYPE", controller.checkInputType("CONTENT_TYPE", mock(MultipartFile.class), "http://uri", "STRING"));
+        // No content type provided but one input supplied.
+        assertEquals(BaseUploadController.CONTENT_TYPE_FILE, controller.checkInputType(null, mock(MultipartFile.class), null, null));
+        assertEquals(BaseUploadController.CONTENT_TYPE_URI, controller.checkInputType(null, null, "http://uri", null));
+        assertEquals(BaseUploadController.CONTENT_TYPE_STRING, controller.checkInputType(null, null, null, "STRING"));
+        // No content type with multiple inputs.
+        assertThrows(IllegalArgumentException.class, () -> controller.checkInputType(null, mock(MultipartFile.class), null, "STRING"));
+        assertThrows(IllegalArgumentException.class, () -> controller.checkInputType(null, mock(MultipartFile.class), "http://uri", "STRING"));
+        assertThrows(IllegalArgumentException.class, () -> controller.checkInputType(null, mock(MultipartFile.class), "http://uri", null));
+        assertThrows(IllegalArgumentException.class, () -> controller.checkInputType(null, null, "http://uri", "STRING"));
+    }
+
+    @Test
+    void testCheckInputTypeWithAcceptedFailure() {
+        assertNull(controller.checkInputType(null, null, null, null, false));
+        assertEquals("", controller.checkInputType("", null, null, null, false));
+        // No content type with multiple inputs.
+        assertNull(controller.checkInputType(null, mock(MultipartFile.class), "http://uri", "STRING", false));
+        assertNull(controller.checkInputType(null, mock(MultipartFile.class), null, "STRING", false));
+        assertNull(controller.checkInputType(null, mock(MultipartFile.class), "http://uri", null, false));
+        assertNull(controller.checkInputType(null, null, "http://uri", "STRING", false));
+    }
+
+    @Test
+    void testInMinimalUI() {
+        var request = mock(HttpServletRequest.class);
+        when(request.getAttribute(IS_MINIMAL)).thenReturn(Boolean.TRUE);
+        assertTrue(controller.isMinimalUI(request));
+        when(request.getAttribute(IS_MINIMAL)).thenReturn(Boolean.FALSE);
+        assertFalse(controller.isMinimalUI(request));
+        when(request.getAttribute(IS_MINIMAL)).thenReturn(null);
+        assertFalse(controller.isMinimalUI(request));
     }
 
 }
